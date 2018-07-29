ff71742f5426e8bb7b1c7ef24086466613a1559e
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-29 20:20:15 +0000

    automatically backed up by learn

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -25,9 +25,7 @@ class Circles extends React.Component {
     this.setState({ circles: newCircles });
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-   return (this.props.color !== nextProps.color);
- }
+
 
   render() {
     const renderCircles = this.state.circles.map((circle, index) =>
